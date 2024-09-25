bec380d56ac7ae208b72b00fa338d2a843d33538
juger1 <gajehaj8@gmail.com>
2024-09-25 12:23:48 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+API_ID = '25695562'
+API_HASH = '0b691c3e86603a7e34aae0b5927d725a'
+BOT_TOKEN = '7083722060:AAEp1Ec33BYPaHiVNZsTZWoe3U251JtuXsA'
+API_URL = 'https://runurl.in/api'
+SUPPORT_GROUP = 'https://t.me/runurl'
+UPDATES_CHANNEL = 'https://t.me/run_url'
+DATABASE_URL = 'mongodb+srv://pabagav476aersmcom:pabagav476aersmcom@cluster0.5jd4dlx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+DATABASE_NAME = 'runurl' #domain name
